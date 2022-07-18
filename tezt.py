734416535da14d668534d5b74980e5c041da1381
Adjeoda <andydogah@gmail.com>
2022-07-18 20:07:06 +0000

    done

--- tezt.py
+++ tezt.py
@@ -1,8 +1,14 @@
-n=int(input())
-a=""
+# n=int(input())
+# a=""
 
-for i in range(n):
-    j=int(input())
-    a+=j
-b=a[::-1]
-print(b)
+# for i in range(n):
+#     j=int(input())
+#     a+=j
+# b=a[::-1]
+# print(b)
+
+def reverse(word):
+    reverse_word=word[::-1]
+    print(reverse_word)
+
+reverse("Andy")
