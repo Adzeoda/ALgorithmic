b486d5da3875d0dceae7c3ee1b9ebe6a53f375c6
Adjeoda <andydogah@gmail.com>
2022-07-18 01:09:33 +0000

    hmn

--- numberGuess.py
+++ numberGuess.py
@@ -6,12 +6,12 @@ num=random.randint(1,100)
 
 guess=int(input("guess the number between 1 and hundred: "))
 
-for i in range(10):
-    if num==guess:
-        print("correct you got it right in attempt no {}" .format(count))
-        break
-    elif num<guess:
-        print("wrong answer, guess higher\n you have{} attempts left: ".format(count+1))
-    else:
-        print("wrong answer, guess higher\n you have {} attempts left".format(count+1))
-    count+=1    
+
+while guess!=num:
+    # guess=input(: )
+    if guess>num:
+        print("wrong answer go again hint go higher: ")
+        guess=input(": ")
+    elif guess<num:
+        print("wrong answer go again hint go lower: ")
+        guess=input(": ")
