d5efa27e4e572a9988dbc3bc647438458ab4759b
Adjeoda <andydogah@gmail.com>
2022-07-21 22:48:07 +0000

    update

--- style.py
+++ style.py
@@ -11,8 +11,10 @@ def style(sentence):
     print(sentence)
     for i in range(x):
         w.pop(x-1)
+        # w.translate([0]," ")
         print(''.join(w))
         x-=1
 
 
-style("the quick brown fox jumos over the lazy dog")
+sentence=input("enter the sentence you want me to slant: ")
+style(sentence)
