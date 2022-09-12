470d60de973c6af67eba3c903b06ee8a555811b7
Adzeoda <80847832+Adzeoda@users.noreply.github.com>
2022-09-12 23:50:32 +0000

    refraction

--- fibunacciSequence.py
+++ fibunacciSequence.py
@@ -1,26 +1,28 @@
 def fibunaaciNth(nth):
-    n1=0
-    n2=1
-    n3=n1+n2
+    n1 = 0
+    n2 = 1
+    n3 = n1+n2
 
-    while n3<nth:
+    while n3 < nth:
         print(n3)
-        n1=n2
-        n2=n3
-        n3=n1+n2
+        n1 = n2
+        n2 = n3
+        n3 = n1+n2
+
 
 def fibunacciN(n):
-    n1=0
-    n2=1
-    n3=n1+n2
-    count=0
+    n1 = 0
+    n2 = 1
+    n3 = n1+n2
+    count = 0
 
-    while count<n:
+    while count < n:
         print(n3)
-        n1=n2
-        n2=n3
-        n3=n1+n2
-        count+=1
+        n1 = n2
+        n2 = n3
+        n3 = n1+n2
+        count += 1
+
 
 fibunaaciNth(100)
 fibunacciN(100)
