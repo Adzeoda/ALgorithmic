21f6ca2b11b5b822efaa1acdac7592bc1ac957ac
Adzeoda <80847832+Adzeoda@users.noreply.github.com>
2022-09-12 23:53:45 +0000

    refractions

--- timeStamp.py
+++ timeStamp.py
@@ -1,20 +1,21 @@
 def request():
-    max=3
-    time=5
-    timeStamps=[1,2,2,5,7]
-    n1=timeStamps[0]
-    n2=timeStamps[1]
-    n3=n1+n2
-    bul=[]
+    max = 3
+    time = 5
+    timeStamps = [1, 2, 2, 5, 7]
+    n1 = timeStamps[0]
+    n2 = timeStamps[1]
+    n3 = n1+n2
+    bul = []
 
-    for i in range(0,7,5):
-        if n3<max:
-            bool=True
+    for i in range(0, 7, 5):
+        if n3 < max:
+            bool = True
             bul.append(bool)
         else:
-            bool=False
+            bool = False
             bul.append(bool)
 
     print(bul)
+
 
 request()
