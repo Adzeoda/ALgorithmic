6596fa141b4d2ed11de5a152ff28930452fac7bc
Adzeoda <80847832+Adzeoda@users.noreply.github.com>
2022-11-30 12:19:34 +0000

    update

--- numberGuess.py
+++ numberGuess.py
@@ -1,18 +1,19 @@
 import random
 
 number=random.randint(0,100)
+print(number)
 guess=int(input("guess the number: "))
 count=1
 
-if count==number:
+if guess==number:
     print("congratulations you guessed the right number")
 
 while guess!= number:
     if guess>number:
-        guess=int(input("wrong guess, you have made{} attempt, guess lower: ".format(count)))
+        guess=int(input("wrong guess, you have made {} attempt(s), guess lower: ".format(count)))
     elif guess<number:
-        guess=int(input("wrong guess, you have made{} attempt, guess  higher: ".format(count)))
+        guess=int(input("wrong guess, you have made {} attempt(s), guess  higher: ".format(count)))
     count+=1
 
-if count==number:
+if guess==number:
     print("congratulations you guessed the right number in your {} attempt". format(count))
