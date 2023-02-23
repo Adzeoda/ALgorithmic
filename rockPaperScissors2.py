d418ca7c6b0faf88be2487d44d0bf3b61acf640d
Adzeoda <80847832+Adzeoda@users.noreply.github.com>
2023-02-23 14:49:31 +0000

    initial commit

--- rockPaperScissors2.py
+++ rockPaperScissors2.py
@@ -1,12 +1,77 @@
 import random
 
-gameObjs=["rock","paper","scissors"]
+vars=["r","p","s"]
+yourscore=0
+opscore=0
+count=1
 
-numberGames=int(input("how many games do you want to play?: "))
-count=0
+def win(yourvar,opsvar):
+    global count
+    global yourscore
+    print("you played {} your opponent played {} you win game {}".format(yourvar,opsvar,count))
+    yourscore+=1
+    print("the scores is {} - {}" .format(yourscore,opscore))
+    if yourscore> opscore:
+        print("In your favor")
+    elif yourscore<opscore:
+        print("in opponents favour")
+    count+=1
 
-while count<numberGames:
-    games=input("enter 1 for rock, 2 for paper, 3 for scissors: ")
-    if games <1 and games>3:
-        print("ENTER A NUMBER FROM 1 TO 3")
-        games=input(" :")
+def lose(yourvar,opsvar):
+    global count
+    global opscore
+    print("you played {} your opponent played {} you lose game {}".format(yourvar,opsvar,count))
+    opscore+=1
+    print("the scores is {} - {}" .format(yourscore,opscore))
+    if yourscore> opscore:
+        print("In your favor")
+    elif yourscore<opscore:
+        print("in opponents favour")
+    count+=1
+
+def draw(yourvar):
+    global count
+    print("you both played {} you draw".format(yourvar))
+    print("the scores is still {} - {}" .format(yourscore,opscore))
+    if yourscore> opscore:
+        print("In your favor")
+    elif yourscore<opscore:
+        print("in opponents favour")
+    count+=1
+
+for i in range(5):
+    print("select 'R' for 'rock', 'P' for 'paper', or 'S' for 'scissors'")
+    yourvar=input(": ")
+    yourvar=yourvar.lower()
+    opsvar=random.choice(vars)
+    
+    if yourvar=="r":
+        if opsvar=="r":
+            draw("r")
+        elif opsvar=="p":
+            lose("r","p")
+        elif opsvar=="s":
+            win("r","s")
+        else:
+            print("invalid respose")
+    
+    elif yourvar=="p":
+        if opsvar=="p":
+            draw("p") 
+        elif opsvar=="s":
+            lose("p","s")
+        elif opsvar=="r":
+            win("p","s")
+        else:
+            print("invalid response")
+
+    elif yourvar=="s":
+        if opsvar=="s":
+            draw(yourvar)
+        elif opsvar=="r":
+            lose(yourvar,opsvar)
+        elif opsvar=="p":
+            win(yourvar,opsvar)
+
+    else:
+        print("INVALID RESPONSE!")
